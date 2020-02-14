f8ef5adae532f3fffe7bdacf72323d34bab1ad8e
Julian Schmidtke <julianschmidtke1998@gmail.com>
2020-02-14 16:16:17 +0100

    Bug fix

--- tomcat.py
+++ tomcat.py
@@ -1,5 +1,4 @@
->>> the_world_is_flat = True
->>> if the_world_is_flat:
-...     print("Be careful not to fall off!")
-...
-Be careful not to fall off!
+print("The Earth is flat")
+the_world_is_flat = True
+if the_world_is_flat:
+     print("Be careful not to fall off!")
