862b7b6162cf7e47cdaf370a79cd89dbdd0218a6
DominikEinfalt98 <dominik.einfalt2@googlemail.com>
2020-02-29 08:47:11 +0100

    basically all of 7

--- tomcat.py
+++ tomcat.py
@@ -141,7 +141,7 @@ groupRemoveWriteWorldRemoveAll = 0o750  # g-w, o-rwx
 worldRemoveAll = 0o770  # o-rwx
 
 # 4.1 Restrict access to $CATALINA_HOME (Scored)
-# os.chown(catalinaHome, tomcatAdmin, tomcatGroup)
+os.chown(catalinaHome, tomcatAdmin, tomcatGroup)
 os.chmod(catalinaHome, groupRemoveWriteWorldRemoveAll)
 # chmod g-w,o-rwx $CATALINA_HOME
 
@@ -149,53 +149,53 @@ os.chmod(catalinaHome, groupRemoveWriteWorldRemoveAll)
 # Not used
 
 # 4.3 Restrict access to Tomcat configuration directory (Scored)
-# os.chown(catalinaHome + '/conf/', tomcatAdmin, tomcatGroup)
+os.chown(catalinaHome + '/conf/', tomcatAdmin, tomcatGroup)
 os.chmod(catalinaHome + '/conf/', groupRemoveWriteWorldRemoveAll)
 
 # 4.4 Restrict access to Tomcat logs directory (Scored)
-# os.chown(catalinaHome + '/logs/', tomcatAdmin, tomcatGroup)
+os.chown(catalinaHome + '/logs/', tomcatAdmin, tomcatGroup)
 os.chmod(catalinaHome + '/logs/', worldRemoveAll)
 
 # 4.5 Restrict access to Tomcat temp directory (Scored)
-# os.chown(catalinaHome + '/temp/', tomcatAdmin, tomcatGroup)
+os.chown(catalinaHome + '/temp/', tomcatAdmin, tomcatGroup)
 os.chmod(catalinaHome + '/temp/', worldRemoveAll)
 
 # 4.6 Restrict access to Tomcat binaries directory (Scored)
-# os.chown(catalinaHome + '/bin/', tomcatAdmin, tomcatGroup)
+os.chown(catalinaHome + '/bin/', tomcatAdmin, tomcatGroup)
 os.chmod(catalinaHome + '/bin/', worldRemoveAll)
 
 # 4.7 Restrict access to Tomcat web application directory (Scored)
-# os.chown(catalinaHome + '/webapps/', tomcatAdmin, tomcatGroup)
+os.chown(catalinaHome + '/webapps/', tomcatAdmin, tomcatGroup)
 os.chmod(catalinaHome + '/webapps/', worldRemoveAll)
 
 # 4.8 Restrict access to Tomcat catalina.policy (Scored)
-# os.chown(catalinaHome + '/conf/catalina.policy', tomcatAdmin, tomcatGroup)
+os.chown(catalinaHome + '/conf/catalina.policy', tomcatAdmin, tomcatGroup)
 
 # 4.9 Restrict access to Tomcat catalina.properties (Scored)
-# os.chown(catalinaHome + '/conf/catalina.properties', tomcatAdmin, tomcatGroup)
+os.chown(catalinaHome + '/conf/catalina.properties', tomcatAdmin, tomcatGroup)
 os.chmod(catalinaHome + '/conf/catalina.properties',
          groupRemoveWriteWorldRemoveAll)
 
 # 4.10 Restrict access to Tomcat context.xml (Scored)
-# os.chown(catalinaHome + '/conf/context.xml', tomcatAdmin, tomcatGroup)
+os.chown(catalinaHome + '/conf/context.xml', tomcatAdmin, tomcatGroup)
 os.chmod(catalinaHome + '/conf/context.xml', groupRemoveWriteWorldRemoveAll)
 
 # 4.11 Restrict access to Tomcat logging.properties (Scored)
-# os.chown(catalinaHome + '/conf/logging.properties', tomcatAdmin, tomcatGroup)
+os.chown(catalinaHome + '/conf/logging.properties', tomcatAdmin, tomcatGroup)
 os.chmod(catalinaHome + '/conf/logging.properties',
          groupRemoveWriteWorldRemoveAll)
 
 # 4.12 Restrict access to Tomcat server.xml (Scored)
-# os.chown(catalinaHome + '/conf/server.xml', tomcatAdmin, tomcatGroup)
+os.chown(catalinaHome + '/conf/server.xml', tomcatAdmin, tomcatGroup)
 os.chmod(catalinaHome + '/conf/server.xml', groupRemoveWriteWorldRemoveAll)
 
 # 4.13 Restrict access to Tomcat tomcat-users.xml (Scored)
-# os.chown(catalinaHome + '/conf/tomcat-users.xml', tomcatAdmin, tomcatGroup)
+os.chown(catalinaHome + '/conf/tomcat-users.xml', tomcatAdmin, tomcatGroup)
 os.chmod(catalinaHome + '/conf/tomcat-users.xml',
          groupRemoveWriteWorldRemoveAll)
 
 # 4.14 Restrict access to Tomcat web.xml (Scored)
-# os.chown(catalinaHome + '/conf/web.xml', tomcatAdmin, tomcatGroup)
+os.chown(catalinaHome + '/conf/web.xml', tomcatAdmin, tomcatGroup)
 os.chmod(catalinaHome + '/conf/web.xml', groupRemoveWriteWorldRemoveAll)
 
 # 5 Configure Realms
@@ -241,43 +241,84 @@ for dir in dirs:
     dstLoggingPropertiesDir = dstLoggingPropertiesDir + '/logging.properties'
     copy2(sourceLoggingPorperties, dstLoggingPropertiesDir)
     # 7.2 Specify file handler in logging.properties files (Scored)
-    line = "handlers = 1catalina.org.apache.juli.FileHandler, java.util.logging.ConsoleHandler"
+    handlerLine = "handlers = 1catalina.org.apache.juli.FileHandler, java.util.logging.ConsoleHandler"
     logfile = open(dstLoggingPropertiesDir)
     loglist = logfile.readlines()
     logfile.close()
-    if line not in loglist:
-        loglist.append(line)
+    if handlerLine not in loglist:
+        loglist.append(handlerLine)
+    # 7.6 Ensure directory in logging.properties is a secure location (Scored)
+    logFileLoc = webAppsDir + dir + '/WEB-INF/'
+    logLocLine = dir + ".org.apache.juli.FileHandler.directory=" + logFileLoc
+    if logLocLine not in loglist:
+        loglist.append(logLocLine)
+    appNameLine = dir + ".org.apache.juli.FileHandler.prefix=" + dir
+    if appNameLine not in loglist:
+        loglist.append(appNameLine)
+    # 7.7 Configure log file size limit (Scored)
+    loglist.append("java.util.logging.FileHandler.limit=10000")
+
     new_logfile = open(dstLoggingPropertiesDir, 'w')
     for lines in loglist:
         new_logfile.write(lines)
+
+    # 7.6 also
+    os.chown(logFileLoc, tomcatAdmin, tomcatGroup)
+    os.chmod(logFileLoc, groupRemoveWriteWorldRemoveAll)
+
     # 7.3 Ensure className is set correctly in context.xml (Scored)
-    # 7.4 Ensure directory in context.xml is a secure location (Scored)
     contextXMLFile = webAppsDir + dir + '/META-INF/context.xml'
     if os.path.exists(contextXMLFile):
         serverTree = elementTree.parse(contextXMLFile)
         serverRoot = serverTree.getroot()
-        connectors = findElementsByTagname(serverRoot, 'Valve')
-        for connector in connectors:
-            if connector.get("className") == "org.apache.catalina.valves.RemoteAddrValve":
-                connector.set("className", "org.apache.catalina.valves.AccessLogValve")
-        realmElement = serverRoot.find("Valve")
-        realmElement.set("directory", "$CATALINA_HOME/logs/")
-        realmElement.set("prefix", "access_log")
-        realmElement.set("fileDateFormat", "yyyy-MM-dd.HH")
-        realmElement.set("suffix", ".log")
-        realmElement.set("pattern", "%t %H cookie:%{SESSIONID}c request:%{SESSIONID}r %m %U %s %q %r")
+        valves = findElementsByTagname(serverRoot, 'Valve')
+        for valve in valves:
+            if valve.get("className") == "org.apache.catalina.valves.RemoteAddrValve":
+                valve.set("className", 'org.apache.catalina.valves.AccessLogValve')
+        # 7.4 Ensure directory in context.xml is a secure location (Scored)
+        # 7.5 Ensure pattern in context.xml is correct (Scored)
+        valveElement = serverRoot.find("Valve")
+        valveElement.set("directory", "$CATALINA_HOME/logs/")
+        valveElement.set("prefix", "access_log")
+        valveElement.set("fileDateFormat", "yyyy-MM-dd.HH")
+        valveElement.set("suffix", ".log")
+        valveElement.set("pattern", "%t %H cookie:%{SESSIONID}c request:%{SESSIONID}r %m %U %s %q %r")
         serverTree.write(contextXMLFile)
 
-# 7.5 Ensure pattern in context.xml is correct (Scored)
-# 7.6 Ensure directory in logging.properties is a secure location (Scored)
-# 7.7 Configure log file size limit (Scored)
+# 7.4 also
+os.chown(catalinaHome + "/logs", tomcatAdmin, tomcatGroup)
+os.chmod(catalinaHome + "/logs", groupRemoveWriteWorldRemoveAll)
 
 # 8 Configure Catalina Policy
 # 8.1 Restrict runtime access to sensitive packages (Scored)
+catalinaConf = catalinaHome + '/conf/catalina.properties'
+packageLine = "package.access = sun.,org.apache.catalina.,org.apache.coyote.,org.apache.tomcat., org.apache.jasper"
+logfile = open(catalinaConf)
+loglist = logfile.readlines()
+logfile.close()
+if packageLine not in loglist:
+    loglist.append(packageLine)
+
+new_logfile = open(catalinaConf, 'w')
+for lines in loglist:
+    new_logfile.write(lines)
 
 # 9 Application Deployment
 # 9.1 Starting Tomcat with Security Manager (Scored)
+# add -security to tomcat startup scrip in /etc/init.d
+#engine -> host -> service -> root autoDeploy = false
 # 9.2 Disabling auto deployment of applications (Scored)
+serverXMLFile = catalinaHome + '/conf/server.xml'
+if os.path.exists(serverXMLFile):
+    serverTree = elementTree.parse(serverXMLFile)
+    serverRoot = serverTree.getroot()
+    service = findElementsByTagname(serverRoot, 'Service')
+    engine = findElementsByTagname(service, 'Engine')
+    hosts = findElementsByTagname(engine, 'Host')
+    for host in hosts:
+        if host.get("autoDeploy") == "true":
+            host.set("autoDeploy", 'false')
+
 # 9.3 Disable deploy on startup of applications (Scored)
 
 # 10 Miscellaneous Configuration Settings
